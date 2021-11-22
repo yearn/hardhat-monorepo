3c6fbc720da98c0fd8e6ab8fdee2f37cd435bde2
Skeletor Spaceman <92943766+skeletor-spaceman@users.noreply.github.com>
2021-11-22 12:23:29 -0300

    feat: manual detached work script update (#28)

--- packages/strategies-keep3r/scripts/jobs/detached/05-v2-jobs-work.ts
+++ packages/strategies-keep3r/scripts/jobs/detached/05-v2-jobs-work.ts
@@ -32,10 +32,21 @@ function promptAndSubmit(): Promise<void | Error> {
       reject(`Error while working tend detached job: ${err.message}`);
     }
 
+    const strategiesToAlwaysHarvest: string[] = [
+      '0xff3AeA00d3d58ba1a3672c766cc5060FfCb8cca3',
+      '0xAB1d2ABbe31FA5945BfA0864f29dadDcB9cd9eAc',
+      '0x399ee38023a914e953ab02ba164ddf941fe39bb9',
+      '0xca25d703cd814e8f5cef9aacee0e7424bb4bccc6',
+      '0x9d32f2bfc5dda3e82f364bf2f8a30d73247a01d1',
+      '0x20b5e014f7251dfee5e51b5d2d627dfa5ff73824',
+    ];
+
     try {
       const harvestCooldown = 10 * 60; // 10 minutes
       const harvestV2DetachedJob = await ethers.getContractAt('HarvestV2DetachedJob', contracts.harvestV2DetachedJob[networkName]);
-      const strategies = await harvestV2DetachedJob.callStatic.strategies();
+      let strategies = await harvestV2DetachedJob.callStatic.strategies();
+      strategies = strategies.filter((strategy: string) => strategiesToAlwaysHarvest.indexOf(strategy) == -1);
+
       console.log(`checking ${strategies.length} harvestV2DetachedJob strategies`);
       let lastWork;
       for (const strategy of strategies) {
@@ -44,10 +55,30 @@ function promptAndSubmit(): Promise<void | Error> {
       }
       if (lastWork.toNumber() + harvestCooldown > Math.floor(new Date().valueOf() / 1000)) {
         console.log('harvest job on cooldown');
-        return resolve();
+      } else {
+        for (const strategy of strategies) {
+          try {
+            const workable = await harvestV2DetachedJob.callStatic.workable(strategy);
+            if (!workable) continue;
+            console.log('working:', strategy);
+            await harvestV2DetachedJob.callStatic.work(strategy);
+            await harvestV2DetachedJob.work(strategy);
+            console.log('worked');
+            return resolve();
+          } catch (error) {
+            console.log(error);
+          }
+        }
       }
+    } catch (err: any) {
+      reject(`Error while working harvest detached job: ${err.message}`);
+    }
 
-      for (const strategy of strategies) {
+    try {
+      const harvestV2DetachedJob = await ethers.getContractAt('HarvestV2DetachedJob', contracts.harvestV2DetachedJob[networkName]);
+      console.log(`checking ${strategiesToAlwaysHarvest.length} harvestV2DetachedJob forced strategies`);
+
+      for (const strategy of strategiesToAlwaysHarvest) {
         try {
           const workable = await harvestV2DetachedJob.callStatic.workable(strategy);
           if (!workable) continue;
@@ -61,8 +92,9 @@ function promptAndSubmit(): Promise<void | Error> {
         }
       }
     } catch (err: any) {
-      reject(`Error while working harvest detached job: ${err.message}`);
+      reject(`Error while force working harvest detached job: ${err.message}`);
     }
+
     console.log('no work');
     return resolve();
   });
