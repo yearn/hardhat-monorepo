4bb3f0f897ee8c6a3d264c5c421ccb258ccb8a05
Skeletor Spaceman <skeletorspaceman@gmail.com>
2021-12-15 17:55:59 -0300

    chore: deploy async sushiswap swapper

--- packages/yswaps/scripts/add-strategy-adder.ts
+++ packages/yswaps/scripts/add-strategy-adder.ts
@@ -1,9 +1,9 @@
 import { run, ethers } from 'hardhat';
 
 async function main() {
-  const strategyAdminToAdd = '0x1ea056c13f8ccc981e51c5f1cdf87476666d0a74';
+  const strategyAdderToAdd = '';
   const tradeFactory = await ethers.getContract('TradeFactory');
-  await tradeFactory.grantRole(await tradeFactory.STRATEGY_ADMIN(), strategyAdminToAdd);
+  await tradeFactory.grantRole(await tradeFactory.STRATEGY_ADDER(), strategyAdderToAdd);
 }
 
 main()
