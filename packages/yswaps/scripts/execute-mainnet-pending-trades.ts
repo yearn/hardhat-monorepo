ad351c04ccf2fabade07bbbf459838276d3f23b3
b0dhidharma <b0dhidharma@protonmail.com>
2022-01-10 17:06:36 -0300

    fix(yswaps): chain id on mainnet execution

--- packages/yswaps/scripts/execute-mainnet-pending-trades.ts
+++ packages/yswaps/scripts/execute-mainnet-pending-trades.ts
@@ -22,9 +22,9 @@ import { getNodeUrl } from '@utils/network';
 import { JsonRpcProvider } from '@ethersproject/providers';
 import * as evm from '@test-utils/evm';
 
-const DELAY = moment.duration('3', 'minutes').as('milliseconds');
-const MAX_GAS_PRICE = utils.parseUnits('200', 'gwei');
-const FLASHBOT_MAX_PRIORITY_FEE_PER_GAS = 2.5;
+const DELAY = moment.duration('8', 'minutes').as('milliseconds');
+const MAX_GAS_PRICE = utils.parseUnits('300', 'gwei');
+const FLASHBOT_MAX_PRIORITY_FEE_PER_GAS = 4;
 const MAX_PRIORITY_FEE_PER_GAS = utils.parseUnits('6', 'gwei');
 
 // Flashbot
@@ -53,7 +53,6 @@ async function main() {
   const ymech = new ethers.Wallet(await kms.decrypt(process.env.MAINNET_1_PRIVATE_KEY as string), ethers.provider);
   await ethers.provider.send('hardhat_setBalance', [ymech.address, '0xffffffffffffffff']);
   console.log('[Setup] Executing with address', ymech.address);
-  // const flashbotsSigner = new ethers.Wallet(await kms.decrypt(process.env.FLASHBOTS_1_PRIVATE_KEY as string));
 
   // We create a provider thats connected to a real network, hardhat provider will be connected to fork
   httpProvider = new ethers.providers.JsonRpcProvider(getNodeUrl('mainnet'), 'mainnet');
@@ -61,7 +60,7 @@ async function main() {
   // console.log('[Setup] Creating flashbots provider ...');
   // flashbotsProvider = await FlashbotsBundleProvider.create(
   //   httpProvider, // a normal ethers.js provider, to perform gas estimiations and nonce lookups
-  //   flashbotsSigner // ethers.js signer wallet, only for signing request payloads, not transactions
+  //   ymech // ethers.js signer wallet, only for signing request payloads, not transactions
   // );
 
   const tradeFactory: TradeFactory = await ethers.getContract('TradeFactory', ymech);
@@ -71,6 +70,8 @@ async function main() {
   for (const id of pendingTradesIds) {
     pendingTrades.push(await tradeFactory.pendingTradesById(id));
   }
+
+  let nonce = await ethers.provider.getTransactionCount(ymech.address);
 
   for (const pendingTrade of pendingTrades) {
     const tokenIn = await ethers.getContractAt<IERC20Metadata>(IERC20_ABI, pendingTrade._tokenIn);
@@ -125,33 +126,42 @@ async function main() {
       pendingTrade._id,
       bestSetup.swapper,
       bestSetup.minAmountOut!,
-      bestSetup.data
-    );
-    const confirmedTx = await simulatedTx.wait();
-    console.log('[Execution] Simulation in fork succeeded used', confirmedTx.gasUsed.toString(), 'gas');
-
-    const populatedTx = await tradeFactory.populateTransaction['execute(uint256,address,uint256,bytes)'](
-      pendingTrade._id,
-      bestSetup.swapper,
-      bestSetup.minAmountOut!,
       bestSetup.data,
       {
-        ...gasParams,
-        gasLimit: confirmedTx.gasUsed.add(confirmedTx.gasUsed.div(10)), // We add a safety net of 10% more of gass
+        nonce,
       }
     );
+    const confirmedTx = await simulatedTx.wait();
+    console.log('[Execution] Simulation in fork succeeded and used', confirmedTx.gasUsed.toString(), 'gas');
 
-    const signedTx = await ymech.signTransaction({
+    const populatedTx = await tradeFactory
+      .connect(httpProvider)
+      .populateTransaction['execute(uint256,address,uint256,bytes)'](
+        pendingTrade._id,
+        bestSetup.swapper,
+        bestSetup.minAmountOut!,
+        bestSetup.data,
+        {
+          nonce,
+          ...gasParams,
+          gasLimit: confirmedTx.gasUsed.add(confirmedTx.gasUsed.div(5)), // We add a safety net of 20% more of gass
+        }
+      );
+
+    const signedTx = await ymech.connect(httpProvider).signTransaction({
       ...gasParams,
       type: 2,
       to: populatedTx.to!,
+      nonce,
       gasLimit: populatedTx.gasLimit!.toNumber(),
       data: populatedTx.data!,
+      chainId: 1,
     });
 
     console.log('[Execution] Sending transaction in block', await httpProvider.getBlockNumber());
     const protectTx = await protect.sendTransaction(signedTx);
-    console.log(`[Execution] Transaction submitted via protect rpc - https://protect.flashbots.net/tx/?hash=${protectTx.hash}`);
+    console.log(`[Execution] Transaction submitted via protect rpc - https://protect.flashbots.net/tx?hash=${protectTx.hash}`);
+    nonce++;
 
     // const bundle: FlashbotBundle = [
     //   {
