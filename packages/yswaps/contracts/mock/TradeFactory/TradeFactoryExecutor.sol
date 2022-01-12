07198df5f518b55fe302cb4ab1d368c200f465ee
b0dhidharma <b0dhidharma@protonmail.com>
2022-01-12 13:18:34 -0300

    chore: renames strategy adder to strategy manager

--- packages/yswaps/contracts/mock/TradeFactory/TradeFactoryExecutor.sol
+++ packages/yswaps/contracts/mock/TradeFactory/TradeFactoryExecutor.sol
@@ -9,9 +9,9 @@ contract TradeFactoryExecutorMock is TradeFactoryPositionsHandlerMock, TradeFact
     address _masterAdmin, 
     address _swapperAdder, 
     address _swapperSetter, 
-    address _strategyAdder, 
+    address _strategyModifier, 
     address _mechanicsRegistry
   ) 
-    TradeFactoryPositionsHandlerMock(_masterAdmin, _swapperAdder, _swapperSetter, _strategyAdder)
+    TradeFactoryPositionsHandlerMock(_masterAdmin, _swapperAdder, _swapperSetter, _strategyModifier)
     TradeFactoryExecutor(_mechanicsRegistry) {}
 }
