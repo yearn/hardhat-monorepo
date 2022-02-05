a59bb5c9874dab1deca2645058795b9a2ce6fb72
Poolpi Tako <poolpitako@protonmail.com>
2022-02-04 18:11:29 -0600

    fix: lint

--- packages/yswaps/contracts/interfaces/IWETH.sol
+++ packages/yswaps/contracts/interfaces/IWETH.sol
@@ -2,6 +2,7 @@ pragma solidity >=0.5.0;
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 interface IWETH is IERC20 {
-    function deposit() external payable;
-    function withdraw(uint256) external;
+  function deposit() external payable;
+
+  function withdraw(uint256) external;
 }
