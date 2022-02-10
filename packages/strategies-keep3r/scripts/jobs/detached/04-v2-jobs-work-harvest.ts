aa4243973142052fe8ba345161955b4454c3a3ed
b0dhidharma <b0dhidharma@protonmail.com>
2022-02-10 20:37:26 -0300

    refactor: remove is on liquidity cooldown references

--- packages/strategies-keep3r/scripts/jobs/detached/04-v2-jobs-work-harvest.ts
+++ packages/strategies-keep3r/scripts/jobs/detached/04-v2-jobs-work-harvest.ts
@@ -2,18 +2,13 @@ import moment from 'moment';
 import { HarvestV2DetachedJob, HarvestV2DetachedJob__factory, IBaseStrategy__factory } from '@typechained';
 import { ethers } from 'hardhat';
 import * as contracts from '../../../utils/contracts';
-import { HarvestConfiguration, harvestConfigurations } from '../../../utils/v2-ftm-strategies';
 import * as gasprice from '../../../utils/ftm-gas-price';
-import { BigNumber, utils } from 'ethers';
+import { utils } from 'ethers';
 
 let harvestV2DetachedJob: HarvestV2DetachedJob;
 const worked: string[] = [];
 const notWorkable: string[] = [];
 const errorWhileWorked: string[] = [];
-const lastTimeRewardWasDumped: { [address: string]: number } = {};
-
-// FIX: This might make lats strats on the array never be worked
-const REWARD_DUMPED_COOLDOWN = moment.duration('0', 'minutes');
 
 async function main() {
   const [harvester] = await ethers.getSigners();
@@ -38,19 +33,6 @@ async function main() {
     timestampBn.toNumber()
   );
 
-  // Map when was the last time a reward token was dumped
-  lastWorksAt.forEach((lastWorkAt: number, i: number) => {
-    const harvestConfiguration: HarvestConfiguration | undefined = harvestConfigurations.find(
-      (harvestConfiguration) => harvestConfiguration.address.toLowerCase() === strategies[i].toLowerCase()
-    );
-    if (!harvestConfiguration) throw new Error('Mismatch between harvests configuration and job strategies');
-    harvestConfiguration.tokensBeingDumped.forEach((tokenBeingDumped) => {
-      if (!lastTimeRewardWasDumped.hasOwnProperty(tokenBeingDumped) || lastWorkAt > lastTimeRewardWasDumped[tokenBeingDumped]) {
-        lastTimeRewardWasDumped[tokenBeingDumped] = lastWorkAt;
-      }
-    });
-  });
-
   // Get all workable
   const workable = await Promise.all(strategies.map((strategy) => harvestV2DetachedJob.workable(strategy)));
 
@@ -70,19 +52,10 @@ async function main() {
     const strategy = strategies[i];
     console.log('[App] Checking strategy', strategy);
     try {
-      const strategyHarvestConfiguration: HarvestConfiguration = harvestConfigurations.find(
-        (harvestConfiguration) => harvestConfiguration.address.toLowerCase() === strategy.toLowerCase()
-      )!;
-      // let isStratOnLiquidityCooldown: boolean = false;
-      // strategyHarvestConfiguration.tokensBeingDumped.forEach((tokenBeingDumped) => {
-      //   isStratOnLiquidityCooldown =
-      //     isStratOnLiquidityCooldown || moment().subtract(REWARD_DUMPED_COOLDOWN).unix() <= lastTimeRewardWasDumped[tokenBeingDumped];
-      // });
       if (!workable[i]) {
         console.log('[App] Not workable');
         console.log('[App] Harvest trigger status is', harvestTrigger[i]);
         console.log('[App] Is it on work cooldown?', onWorkCooldown[i]);
-        // console.log('[App] Is it on liquidity cooldown?', isStratOnLiquidityCooldown);
         notWorkable.push(strategy);
       } else {
         console.log('[App] Working...');
@@ -94,9 +67,6 @@ async function main() {
         const tx = await harvestV2DetachedJob.work(strategy, {
           gasLimit: gasLimit.mul(110).div(100),
           gasPrice: utils.parseUnits(`${gasprice.get()}`, 'gwei'),
-        });
-        strategyHarvestConfiguration.tokensBeingDumped.forEach((tokenBeingDumped) => {
-          lastTimeRewardWasDumped[tokenBeingDumped] = moment().unix();
         });
         worked.push(strategy);
         console.log(`[App] Check work tx at https://ftmscan.com/tx/${tx.hash} at ${moment()} (${moment().unix()})`);
