c102bcc228cf6543540f705f1d17bdbcc2e17973
gotzenx <78360669+gotzenx@users.noreply.github.com>
2022-02-18 16:09:53 -0300

    feat: trade config typing [MCH-242] (#110)

--- packages/yswaps/scripts/configs/mainnet.ts
+++ packages/yswaps/scripts/configs/mainnet.ts
@@ -4,14 +4,16 @@ import { CurveSpellEth } from '@scripts/libraries/solvers/multicall/CurveSpellEt
 import { CurveYfiEth } from '@scripts/libraries/solvers/multicall/CurveYfiEth';
 import { ThreePoolCrv } from '@scripts/libraries/solvers/multicall/ThreePoolCrv';
 
-const solvers: SolversMap = {
+export type MainnetSolvers = 'CurveSpellEth' | 'CurveYfiEth' | 'ThreePoolCrv' | 'Dexes';
+
+const mainnetSolversMap: SolversMap<'MAINNET'> = {
   CurveSpellEth: new CurveSpellEth(),
   CurveYfiEth: new CurveYfiEth(),
   ThreePoolCrv: new ThreePoolCrv(),
   Dexes: new Dexes(),
 };
 
-const config: StrategyConfiguration = {
+const mainnetConfig: StrategyConfiguration<'MAINNET'> = {
   // '0x91C3424A608439FBf3A91B6d954aF0577C1B9B8A': {
   //   name: 'Strategy that uses 3poolCRV Solver',
   //   tradesConfigurations: [
@@ -64,4 +66,4 @@ const config: StrategyConfiguration = {
   },
 };
 
-export { config, solvers };
+export { mainnetConfig, mainnetSolversMap };
