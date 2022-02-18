f7b24b02eb53e0d539b6dccaf5b2af0981b47bae
gotzenx <78360669+gotzenx@users.noreply.github.com>
2022-02-18 16:09:53 -0300

    [MCH-241-110] feat: trade config typing [MCH-242] (#110)
    * feat: trade config typing
    
    * refactor: mainnetConfig naming implemented
    
    * fix: linting

---
+++ packages/yswaps/scripts/configs/fantom.ts
@@ -0,0 +1,11 @@
+// import { SolversMap, StrategyConfiguration } from '@libraries/types';
+
+// export type FantomSolvers = 'add_solvers_name_here';
+
+// const fantomSolversMap: SolversMap<"FANTOM"> = {
+// };
+
+// const config: StrategyConfiguration<"FANTOM"> = {
+// };
+
+// export { config, fantomSolversMap };
