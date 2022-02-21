c13bbb6ddc368dc6bb936ddc5d2a5e779094fea3
gotzenx <78360669+gotzenx@users.noreply.github.com>
2022-02-21 17:24:27 -0300

    refactor: three pool crv solver (#111)

--- packages/yswaps/scripts/libraries/solvers/multicall/ThreePoolCrv.ts
+++ packages/yswaps/scripts/libraries/solvers/multicall/ThreePoolCrv.ts
@@ -7,6 +7,8 @@ import { SimpleEnabledTrade, Solver } from '@scripts/libraries/types';
 import * as wallet from '@test-utils/wallet';
 import { ethers } from 'hardhat';
 
+const DUST_THRESHOLD = utils.parseEther('1');
+
 // 1) 3pool => [usdc|usdt|dai]
 // 2) [usdc|usdt|dai] => yvBOOST
 // 3) yvBOOST withdraw  => yveCRV
@@ -24,7 +26,7 @@ export class ThreePoolCrv implements Solver {
     if (trades.length != 1) return false;
     const threeCrv = IERC20__factory.connect(this.threeCrvAddress, wallet.generateRandom());
     const strategyBalance = await threeCrv.balanceOf(strategy);
-    return strategyBalance.gt(0);
+    return strategyBalance.gt(DUST_THRESHOLD);
   }
 
   async solve({
@@ -49,6 +51,9 @@ export class ThreePoolCrv implements Solver {
     const yvBoostVault = IVault__factory.connect(this.yvBoostAddress, multicallSwapperSigner);
     const yveCrvToken = IERC20__factory.connect(this.yveCrvAddress, multicallSwapperSigner);
 
+    // Create txs for multichain swapper
+    const transactions: PopulatedTransaction[] = [];
+
     const amount = await threeCrv.balanceOf(strategy);
     console.log('[ThreePoolCrv] 3crv transfer to swapper');
     await threeCrv.transfer(multicallSwapperAddress, amount);
@@ -56,7 +61,11 @@ export class ThreePoolCrv implements Solver {
     // Withdraw usdc from crv3Pool
     console.log('[ThreePoolCrv] Remove liqudity from curve pool');
     const usdcBalancePre = await usdc.balanceOf(multicallSwapperAddress);
-    await crv3Pool.remove_liquidity_one_coin(amount, 1, 0);
+
+    const removeLiquidityTx = await crv3Pool.populateTransaction.remove_liquidity_one_coin(amount, 1, 0);
+    await multicallSwapperSigner.sendTransaction(removeLiquidityTx);
+    transactions.push(removeLiquidityTx);
+
     const usdcBalanceTotal = await usdc.balanceOf(multicallSwapperAddress);
     let usdcBalance = usdcBalanceTotal.sub(usdcBalancePre);
     if (usdcBalanceTotal.eq(usdcBalance)) {
@@ -64,12 +73,13 @@ export class ThreePoolCrv implements Solver {
       usdcBalance = usdcBalance.sub(1);
     }
     console.log(
-      '[ThreePoolCrv] Total USDC after removing liquidity form curve pool',
+      '[ThreePoolCrv] Total USDC after removing liquidity from curve pool',
       utils.formatUnits(usdcBalance, 6),
       `(raw: ${usdcBalance.toString()})`
     );
 
     // Trade USDC for yvBOOST in zrx
+    console.log('[ThreePoolCrv] Getting USDC => yvBOOST trade information via zrx');
     const { data: zrxData, allowanceTarget: zrxAllowanceTarget } = await zrx.quote({
       chainId: 1,
       sellToken: this.usdcAddress,
@@ -78,49 +88,34 @@ export class ThreePoolCrv implements Solver {
       slippagePercentage: 10 / 100,
     });
 
-    console.log('[ThreePoolCrv] Got quote from ZRX');
+    const approveUsdc = (await usdc.allowance(multicallSwapperAddress, zrxAllowanceTarget)).lt(usdcBalance);
+    if (approveUsdc) {
+      console.log('[ThreePoolCrv] Approving usdc');
+      const approveUsdcTx = await usdc.populateTransaction.approve(zrxAllowanceTarget, constants.MaxUint256);
+      await multicallSwapperSigner.sendTransaction(approveUsdcTx);
+      transactions.push(approveUsdcTx);
+    }
 
-    const tx = {
+    console.log('[ThreePoolCrv] Executing USDC => yvBOOST via zrx');
+    const swapTx = {
       to: this.zrxContractAddress,
       data: zrxData,
     };
-
-    const approveUsdc = (await usdc.allowance(multicallSwapperAddress, zrxAllowanceTarget)) < usdcBalance;
-    if (approveUsdc) {
-      console.log('[ThreePoolCrv] Approving usdc');
-      await usdc.approve(zrxAllowanceTarget, constants.MaxUint256);
-    }
-
-    console.log('[ThreePoolCrv] Executing ZRX swap');
-    await multicallSwapperSigner.sendTransaction(tx);
+    await multicallSwapperSigner.sendTransaction(swapTx);
+    transactions.push(swapTx);
 
     const yvBoostBalance: BigNumber = await yvBoostToken.balanceOf(multicallSwapperAddress);
     console.log('[ThreePoolCrv] yvBOOST after swap: ', utils.formatEther(yvBoostBalance), `(raw: ${yvBoostBalance.toString()})`);
 
     console.log('[ThreePoolCrv] Withdrawing yvBOOST');
-    await yvBoostVault.withdraw(constants.MaxUint256, multicallSwapperAddress, 0);
+    const yvBoostWithdrawTx = await yvBoostVault.populateTransaction.withdraw(constants.MaxUint256, multicallSwapperAddress, 0);
+    await multicallSwapperSigner.sendTransaction(yvBoostWithdrawTx);
+    transactions.push(yvBoostWithdrawTx);
 
     const yveCrvBalance: BigNumber = await yveCrvToken.balanceOf(multicallSwapperAddress);
     console.log('[ThreePoolCrv] yveCRV after withdraw', utils.formatEther(yveCrvBalance), `(raw: ${yveCrvBalance.toString()})`);
 
-    // Create txs for multichain swapper
-    const transactions: PopulatedTransaction[] = [];
-
-    // 1) Withdraw usdc from 3pool
-    transactions.push(await crv3Pool.populateTransaction.remove_liquidity_one_coin(amount, 1, 0));
-
-    // 2) Approve usdc in zrx (if neccesary)
-    if (approveUsdc) transactions.push(await usdc.populateTransaction.approve(zrxAllowanceTarget, constants.MaxUint256));
-
-    // 3) Swap usdc for yvBOOST
-    transactions.push(tx);
-
-    // 4) Withdraw from yvBOOST
-    transactions.push(await yvBoostVault.populateTransaction.withdraw(constants.MaxUint256, this.strategyAddress, BigNumber.from('0')));
-
     const data: string = mergeTransactions(transactions);
-
-    console.log('[ThreePoolCrv] Data after merging transactions:', data);
 
     const executeTx = await tradeFactory.populateTransaction['execute((address,address,address,uint256,uint256),address,bytes)'](
       {
