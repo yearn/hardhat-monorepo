24c9141fd058fc497a4cb335b089ad2aa9076df9
b0dhidharma <b0dhidharma@protonmail.com>
2022-02-24 15:51:55 -0300

    fix: signer on solvers

--- packages/yswaps/scripts/libraries/solvers/multicall/CurveYfiEth.ts
+++ packages/yswaps/scripts/libraries/solvers/multicall/CurveYfiEth.ts
@@ -1,3 +1,4 @@
+import { JsonRpcSigner } from '@ethersproject/providers';
 import { BigNumber, constants, PopulatedTransaction, Signer, utils } from 'ethers';
 import { ICurveFi, ICurveFi__factory, IERC20, IERC20__factory, IWETH, IWETH__factory, TradeFactory } from '@typechained';
 import zrx from '@libraries/dexes/zrx';
@@ -5,7 +6,7 @@ import { mergeTransactions } from '@scripts/libraries/utils/multicall';
 import { impersonate } from '@test-utils/wallet';
 import { SimpleEnabledTrade, Solver } from '@scripts/libraries/types';
 import { ethers } from 'hardhat';
-import { ITradeFactoryExecutor } from '../../../../typechained/ITradeFactory';
+import { AsyncTradeExecutionDetailsStruct } from '@typechained/ITradeFactoryExecutor';
 
 const DUST_THRESHOLD = utils.parseEther('1');
 
@@ -15,9 +16,9 @@ const DUST_THRESHOLD = utils.parseEther('1');
 // 4) eth => yfi/eth with curve
 
 export class CurveYfiEth implements Solver {
-  private strategyAddress = '0xa04947059831783C561e59A43B93dCB5bEE7cab2';
   private zrxContractAddress = '0xDef1C0ded9bec7F1a1670819833240f027b25EfF';
 
+  private _multicallSigner!: JsonRpcSigner;
   private _crvYfiEth!: IERC20;
   private _crvSwap!: ICurveFi;
   private _cvx!: IERC20;
@@ -31,11 +32,21 @@ export class CurveYfiEth implements Solver {
   }
 
   private async loadContracts(): Promise<void> {
-    this._crvYfiEth = await ethers.getContractAt<IERC20>(IERC20__factory.abi, '0x29059568bB40344487d62f7450E78b8E6C74e0e5');
-    this._cvx = await ethers.getContractAt<IERC20>(IERC20__factory.abi, '0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B');
-    this._crv = await ethers.getContractAt<IERC20>(IERC20__factory.abi, '0xD533a949740bb3306d119CC777fa900bA034cd52');
-    this._weth = await ethers.getContractAt<IWETH>(IWETH__factory.abi, '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2');
-    this._crvSwap = await ethers.getContractAt<ICurveFi>(ICurveFi__factory.abi, '0xC26b89A667578ec7b3f11b2F98d6Fd15C07C54ba');
+    const multicallSwapperAddress = (await ethers.getContract('MultiCallOptimizedSwapper')).address;
+    this._multicallSigner = await impersonate(multicallSwapperAddress);
+    this._crvYfiEth = await ethers.getContractAt<IERC20>(
+      IERC20__factory.abi,
+      '0x29059568bB40344487d62f7450E78b8E6C74e0e5',
+      this._multicallSigner
+    );
+    this._cvx = await ethers.getContractAt<IERC20>(IERC20__factory.abi, '0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B', this._multicallSigner);
+    this._crv = await ethers.getContractAt<IERC20>(IERC20__factory.abi, '0xD533a949740bb3306d119CC777fa900bA034cd52', this._multicallSigner);
+    this._weth = await ethers.getContractAt<IWETH>(IWETH__factory.abi, '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', this._multicallSigner);
+    this._crvSwap = await ethers.getContractAt<ICurveFi>(
+      ICurveFi__factory.abi,
+      '0xC26b89A667578ec7b3f11b2F98d6Fd15C07C54ba',
+      this._multicallSigner
+    );
   }
 
   async shouldExecuteTrade({ strategy, trades }: { strategy: string; trades: SimpleEnabledTrade[] }): Promise<boolean> {
@@ -53,9 +64,7 @@ export class CurveYfiEth implements Solver {
     trades: SimpleEnabledTrade[];
     tradeFactory: TradeFactory;
   }): Promise<PopulatedTransaction> {
-    const multicallSwapperAddress = (await ethers.getContract('MultiCallOptimizedSwapper')).address;
-    const strategySigner = await impersonate(this.strategyAddress);
-    const multicallSwapperSigner = await impersonate(multicallSwapperAddress);
+    const strategySigner = await impersonate(strategy);
 
     const crvBalance = (await this._crv.balanceOf(strategy)).sub(1);
     console.log('[CurveYfiEth] Total CRV balance is', utils.formatEther(crvBalance));
@@ -66,14 +75,14 @@ export class CurveYfiEth implements Solver {
     const transactions: PopulatedTransaction[] = [];
 
     // Create list of trades details to be executed
-    const asyncTradesExecutionDetails: ITradeFactoryExecutor.AsyncTradeExecutionDetailsStruct[] = [];
+    const asyncTradesExecutionDetails: AsyncTradeExecutionDetailsStruct[] = [];
 
     console.log('[CurveYfiEth] CRV management');
     const shouldExecuteCrvTrade = (await this._crv.balanceOf(strategy)).gt(DUST_THRESHOLD);
 
     if (shouldExecuteCrvTrade) {
       console.log('[CurveYfiEth] Transfering crv to multicall swapper for simulations');
-      await this._crv.connect(strategySigner).transfer(multicallSwapperAddress, crvBalance);
+      await this._crv.connect(strategySigner).transfer(this._multicallSigner._address, crvBalance);
 
       // Do gas optimizations
       console.log('[CurveYfiEth] Getting crv => weth trade information via zrx');
@@ -85,11 +94,11 @@ export class CurveYfiEth implements Solver {
         slippagePercentage: 10 / 100,
       });
 
-      const approveCrv = (await this._crv.allowance(multicallSwapperAddress, zrxCrvAllowanceTarget)).lt(crvBalance);
+      const approveCrv = (await this._crv.allowance(this._multicallSigner._address, zrxCrvAllowanceTarget)).lt(crvBalance);
       if (approveCrv) {
         console.log('[CurveYfiEth] Approving crv');
         const approveCrvTx = await this._crv.populateTransaction.approve(zrxCrvAllowanceTarget, constants.MaxUint256);
-        await multicallSwapperSigner.sendTransaction(approveCrvTx);
+        await this._multicallSigner.sendTransaction(approveCrvTx);
         transactions.push(approveCrvTx);
       }
 
@@ -98,11 +107,11 @@ export class CurveYfiEth implements Solver {
         to: this.zrxContractAddress,
         data: zrxCrvData,
       };
-      await multicallSwapperSigner.sendTransaction(crvToWethTx);
+      await this._multicallSigner.sendTransaction(crvToWethTx);
       transactions.push(crvToWethTx);
 
       asyncTradesExecutionDetails.push({
-        _strategy: this.strategyAddress,
+        _strategy: strategy,
         _tokenIn: this._crv.address,
         _tokenOut: this._crvYfiEth.address,
         _amount: crvBalance,
@@ -117,7 +126,7 @@ export class CurveYfiEth implements Solver {
 
     if (shouldExecuteCvxTrade) {
       console.log('[CurveYfiEth] Transfering cvx to multicall swapper for simulations');
-      await this._cvx.connect(strategySigner).transfer(multicallSwapperAddress, cvxBalance);
+      await this._cvx.connect(strategySigner).transfer(this._multicallSigner._address, cvxBalance);
 
       console.log('[CurveYfiEth] Getting cvx => weth trade information via zrx');
       const { data: zrxCvxData, allowanceTarget: zrxCvxAllowanceTarget } = await zrx.quote({
@@ -128,11 +137,11 @@ export class CurveYfiEth implements Solver {
         slippagePercentage: 10 / 100,
       });
 
-      const approveCvx = (await this._cvx.allowance(multicallSwapperAddress, zrxCvxAllowanceTarget)).lt(cvxBalance);
+      const approveCvx = (await this._cvx.allowance(this._multicallSigner._address, zrxCvxAllowanceTarget)).lt(cvxBalance);
       if (approveCvx) {
         console.log('[CurveYfiEth] Approving cvx');
         const approveCvxTx = await this._cvx.populateTransaction.approve(zrxCvxAllowanceTarget, constants.MaxUint256);
-        await multicallSwapperSigner.sendTransaction(approveCvxTx);
+        await this._multicallSigner.sendTransaction(approveCvxTx);
         transactions.push(approveCvxTx);
       }
 
@@ -141,11 +150,11 @@ export class CurveYfiEth implements Solver {
         to: this.zrxContractAddress,
         data: zrxCvxData,
       };
-      await multicallSwapperSigner.sendTransaction(cvxToWethTx);
+      await this._multicallSigner.sendTransaction(cvxToWethTx);
       transactions.push(cvxToWethTx);
 
       asyncTradesExecutionDetails.push({
-        _strategy: this.strategyAddress,
+        _strategy: strategy,
         _tokenIn: this._cvx.address,
         _tokenOut: this._crvYfiEth.address,
         _amount: cvxBalance,
@@ -156,14 +165,14 @@ export class CurveYfiEth implements Solver {
     }
 
     // WETH management
-    const wethBalance = await this._weth.balanceOf(multicallSwapperAddress);
+    const wethBalance = await this._weth.balanceOf(this._multicallSigner._address);
     console.log('[CurveYfiEth] Total WETH balance is', utils.formatEther(wethBalance));
 
-    const approveWeth = (await this._weth.allowance(multicallSwapperAddress, this._crvSwap.address)).lt(wethBalance);
+    const approveWeth = (await this._weth.allowance(this._multicallSigner._address, this._crvSwap.address)).lt(wethBalance);
     if (approveWeth) {
       console.log('[CurveYfiEth] Approving weth');
       const approveWethTx = await this._weth.populateTransaction.approve(this._crvSwap.address, constants.MaxUint256);
-      await multicallSwapperSigner.sendTransaction(approveWethTx);
+      await this._multicallSigner.sendTransaction(approveWethTx);
       transactions.push(approveWethTx);
     }
 
@@ -174,12 +183,12 @@ export class CurveYfiEth implements Solver {
       [wethBalance, 0],
       curveCalculatedTokenMinAmountOut,
       false,
-      this.strategyAddress
+      strategy
     );
-    await multicallSwapperSigner.sendTransaction(addLiquidityTx);
+    await this._multicallSigner.sendTransaction(addLiquidityTx);
     transactions.push(addLiquidityTx);
 
-    const amountOut = await this._crvYfiEth.balanceOf(this.strategyAddress);
+    const amountOut = await this._crvYfiEth.balanceOf(strategy);
     console.log('[CurveYfiEth] Final crvYFIETH balance is', utils.formatEther(amountOut));
 
     if (amountOut.eq(0)) throw new Error('No crvYfiEth tokens were received');
@@ -194,18 +203,22 @@ export class CurveYfiEth implements Solver {
     return await this.getExecuteTx({
       tradeFactory,
       asyncTradesExecutionDetails,
-      swapperAddress: multicallSwapperAddress,
+      swapperAddress: this._multicallSigner._address,
       data,
     });
   }
 
-  private async getExecuteTx({tradeFactory, asyncTradesExecutionDetails, swapperAddress, data}: {
-    tradeFactory: TradeFactory,
-    asyncTradesExecutionDetails: ITradeFactoryExecutor.AsyncTradeExecutionDetailsStruct[],
-    swapperAddress: string,
-    data: string,
+  private async getExecuteTx({
+    tradeFactory,
+    asyncTradesExecutionDetails,
+    swapperAddress,
+    data,
+  }: {
+    tradeFactory: TradeFactory;
+    asyncTradesExecutionDetails: AsyncTradeExecutionDetailsStruct[];
+    swapperAddress: string;
+    data: string;
   }): Promise<PopulatedTransaction> {
-
     if (!asyncTradesExecutionDetails.length) throw new Error('None trades should be execute');
 
     if (asyncTradesExecutionDetails.length == 1) {
