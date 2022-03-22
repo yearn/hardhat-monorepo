0f0d13f5b0949322e4eb9ae2ebd188c5ac033011
gotzenx <gotzenlenek@gmail.com>
2022-03-22 16:44:51 -0300

    refactor: partial dexes solver

--- packages/yswaps/scripts/libraries/solvers/Dexes.ts
+++ packages/yswaps/scripts/libraries/solvers/Dexes.ts
@@ -1,13 +1,14 @@
 import { ethers } from 'hardhat';
-import { SimpleEnabledTrade, Solver } from '../types';
+import { DexLibrarySwapResponse, SimpleEnabledTrade, Solver } from '../types';
 import * as uniswapV2Library from '@libraries/dexes/uniswap-v2';
-import zrx from '@libraries/dexes/zrx';
+// import zrx from '@libraries/dexes/zrx';
 import { UNISWAP_V2_FACTORY, UNISWAP_V2_ROUTER } from '@deploy/mainnet-swappers/uniswap_v2';
 import { shouldExecuteTrade } from '@scripts/libraries/utils/should-execute-trade';
 import { IERC20Metadata__factory, TradeFactory } from '@typechained';
 import { PopulatedTransaction, utils } from 'ethers';
 import * as wallet from '@test-utils/wallet';
 import { NETWORK_NAME_IDS, SUPPORTED_NETWORKS } from '../../../../commons/utils/network';
+import { dexesNerworkMap, TEST_SUPPORTED } from '../utils/dexes-libraries';
 
 export default class Dexes implements Solver {
   async shouldExecuteTrade({ strategy, trade }: { strategy: string; trade: SimpleEnabledTrade }): Promise<boolean> {
@@ -39,32 +40,57 @@ export default class Dexes implements Solver {
 
     console.log('[Dexes] Total balance is', utils.formatUnits(amount, inDecimals), inSymbol);
     console.log('[Dexes] Getting', inSymbol, '=>', outSymbol, 'trade information');
-    const network: SUPPORTED_NETWORKS = process.env.HARDHAT_DEPLOY_FORK as SUPPORTED_NETWORKS;
-    const { data: zrxData, minAmountOut: zrxMinAmountOut } = await zrx.quote({
-      chainId: NETWORK_NAME_IDS[network],
-      sellToken: tokenInAddress,
-      buyToken: tokenOutAddress,
-      sellAmount: amount,
-      slippagePercentage: 1 / 100,
-      skipValidation: true,
-      takerAddress: strategy,
-    });
 
-    console.log('[Dexes] Calculated min amount', utils.formatEther(zrxMinAmountOut!), outSymbol);
-    const executeTx = await tradeFactory.populateTransaction['execute((address,address,address,uint256,uint256),address,bytes)'](
-      {
-        _strategy: strategy,
-        _tokenIn: tokenInAddress,
-        _tokenOut: tokenOutAddress,
-        _amount: amount,
-        _minAmountOut: zrxMinAmountOut!,
-      },
-      zrxSwapper.address,
-      zrxData
-    );
+    const network = process.env.HARDHAT_DEPLOY_FORK as TEST_SUPPORTED;
+    const dexes = dexesNerworkMap[network];
 
-    if (zrxMinAmountOut!.eq(0)) throw new Error(`No ${outSymbol} tokens were received`);
+    let bestDexResponse: DexLibrarySwapResponse | undefined;
 
-    return executeTx;
+    for (const dex of Object.values(dexes)) {
+      const response = await dex.swap({
+        amountIn: amount,
+        strategy,
+        tokenIn: tokenInAddress,
+        tokenOut: tokenOutAddress,
+      });
+
+      if (!bestDexResponse || response.amountOut.gt(bestDexResponse.amountOut)) {
+        bestDexResponse = response;
+      }
+    }
+
+
+
+    return await tradeFactory.populateTransaction.ETH_ADDRESS();
+
+    // console.log('[Dexes] Total balance is', utils.formatUnits(amount, inDecimals), inSymbol);
+    // console.log('[Dexes] Getting', inSymbol, '=>', outSymbol, 'trade information');
+    // const network: SUPPORTED_NETWORKS = process.env.HARDHAT_DEPLOY_FORK as SUPPORTED_NETWORKS;
+    // const { data: zrxData, minAmountOut: zrxMinAmountOut } = await zrx.quote({
+    //   chainId: NETWORK_NAME_IDS[network],
+    //   sellToken: tokenInAddress,
+    //   buyToken: tokenOutAddress,
+    //   sellAmount: amount,
+    //   slippagePercentage: 1 / 100,
+    //   skipValidation: true,
+    //   takerAddress: strategy,
+    // });
+
+    // console.log('[Dexes] Calculated min amount', utils.formatEther(zrxMinAmountOut!), outSymbol);
+    // const executeTx = await tradeFactory.populateTransaction['execute((address,address,address,uint256,uint256),address,bytes)'](
+    //   {
+    //     _strategy: strategy,
+    //     _tokenIn: tokenInAddress,
+    //     _tokenOut: tokenOutAddress,
+    //     _amount: amount,
+    //     _minAmountOut: zrxMinAmountOut!,
+    //   },
+    //   zrxSwapper.address,
+    //   zrxData
+    // );
+
+    // if (zrxMinAmountOut!.eq(0)) throw new Error(`No ${outSymbol} tokens were received`);
+
+    // return executeTx;
   }
 }
---
+++ packages/yswaps/scripts/libraries/utils/dexes-libraries.ts
@@ -0,0 +1,35 @@
+import { ethers } from "hardhat";
+import { ZrxLibrary } from "../dexes/zrx";
+import { DexLibrary } from "../types";
+
+
+
+const zrxLibrary = new ZrxLibrary({name: 'ZrxLibrary', network: ethers.provider.network });
+
+export type TEST_SUPPORTED = 'mainnet' | 'fantom' | 'arbitrum';
+export type MAINNET_DEXES = 'uniswap' | 'zrx';
+export type FANTOM_DEXES = 'spookyswap' | 'zrx';
+export type ABITRUM_DEXES = 'arbiswap';
+
+export type DexesNetworkMap = {
+  mainnet: {[dexes in MAINNET_DEXES]: DexLibrary};
+  fantom: {[dexes in FANTOM_DEXES]: DexLibrary};
+  arbitrum: {[dexes in ABITRUM_DEXES]: DexLibrary};
+}
+
+export async function initDexesNetworkMap() {
+}
+
+export const dexesNerworkMap: DexesNetworkMap = {
+  fantom: {
+    spookyswap: zrxLibrary,
+    zrx: zrxLibrary,
+  },
+  mainnet: {
+    uniswap: zrxLibrary,
+    zrx: zrxLibrary,
+  },
+  arbitrum: {
+    arbiswap: zrxLibrary
+  }
+}
