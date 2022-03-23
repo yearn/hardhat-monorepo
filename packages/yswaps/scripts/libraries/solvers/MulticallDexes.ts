2d99a154805f2fe77d5d7bedcc323fdf3fe5a18a
gotzenx <gotzenlenek@gmail.com>
2022-03-23 15:08:23 -0300

    feat: first mullticall solver draft

--- packages/yswaps/scripts/libraries/solvers/MulticallDexes.ts
+++ packages/yswaps/scripts/libraries/solvers/MulticallDexes.ts
@@ -6,6 +6,7 @@ import { PopulatedTransaction, utils } from 'ethers';
 import * as wallet from '@test-utils/wallet';
 import { NETWORK_NAME_IDS, SUPPORTED_NETWORKS } from '../../../../commons/utils/network';
 import { dexesNerworkMapMock, SUPPORTED_NETWORKS_MOCK } from '../utils/dexes-libraries-mock';
+import { mergeTransactions } from '../utils/multicall';
 
 export type MultiDexesSolverMetadata = {
   hopTokens: string[];
@@ -27,6 +28,9 @@ export default class MulticallDexes implements Solver {
     tradeFactory: TradeFactory;
   }): Promise<PopulatedTransaction> {
     const { tokenIn: tokenInAddress, tokenOut: tokenOutAddress } = trade;
+    const { hopTokens } =  metadata;
+    if (!hopTokens.length) throw new Error('At least one hop token is required to use MulticallDexes');
+
     const [tokenIn, tokenOut] = await Promise.all([
       IERC20Metadata__factory.connect(tokenInAddress, tradeFactory.signer),
       IERC20Metadata__factory.connect(tokenOutAddress, tradeFactory.signer),
@@ -43,55 +47,76 @@ export default class MulticallDexes implements Solver {
     console.log('[Dexes] Total balance is', utils.formatUnits(amount, inDecimals), inSymbol);
     console.log('[Dexes] Getting', inSymbol, '=>', outSymbol, 'trade information');
 
-    return await tokenIn.populateTransaction.decimals(); // MOCKED
+    const network = process.env.HARDHAT_DEPLOY_FORK as SUPPORTED_NETWORKS_MOCK;
+    const dexes = Object.values(dexesNerworkMapMock[network]);
+    const dexesBestResults: DexLibrarySwapResponse[] = []; // index will represent the path order.
 
-    // const network = process.env.HARDHAT_DEPLOY_FORK as SUPPORTED_NETWORKS_MOCK;
-    // const dexes = dexesNerworkMapMock[network];
+    // pair in: tokenIn + hopToken
+    for (const hopToken of hopTokens) {
+      for (const dex of dexes) {
+        const response = await dex.swap({
+          tokenIn: tokenInAddress,
+          tokenOut: hopToken,
+          amountIn: amount,
+          strategy,
+        });
 
-    // let bestDexResponse: DexLibrarySwapResponse | undefined;
+        const bestResponse = dexesBestResults[0];
+        if (!bestResponse || response.amountOut.gt(bestResponse.amountOut)) {
+          dexesBestResults[0] = response;
+        };
+      };
+    };
 
-    // for (const dex of Object.values(dexes)) {
-    //   const response = await dex.swap({
-    //     amountIn: amount,
-    //     strategy,
-    //     tokenIn: tokenInAddress,
-    //     tokenOut: tokenOutAddress,
-    //     hops: metadata.hopTokens,
-    //   });
+    // pair out: hopToken + tokenOut
+    for (const hopToken of hopTokens) {
+      const firstPathResponse = dexesBestResults[0];
+      for (const dex of dexes) {
+        const response = await dex.swap({
+          tokenIn: hopToken,
+          tokenOut: tokenOutAddress,
+          amountIn: firstPathResponse.amountOut,
+          strategy,
+        });
 
-    //   if (!bestDexResponse || response.amountOut.gt(bestDexResponse.amountOut)) {
-    //     bestDexResponse = response;
-    //   }
-    // }
+        const bestResponse = dexesBestResults[1];
+        if (!bestResponse || response.amountOut.gt(bestResponse.amountOut)) {
+          dexesBestResults[1] = response;
+        };
+      };
+    };
 
-    // if (!bestDexResponse) throw new Error('No valid response from dexes');
+    // check if both paths uses same dex.
+    // If so, merge them into one.
 
-    // const { amountOut, dex, path, swapperData, unsignedSwapTx, swapperAddress } = bestDexResponse;
-    // console.log(
-    //   `[Dexes] Best calculate amount out`,
-    //   utils.formatUnits(amountOut, outDecimals),
-    //   outSymbol,
-    //   ` from dex: ${dex} using path ${path}`
-    // );
+    const firstSwapResponse = dexesBestResults[0];
+    const lastSwapResponse = dexesBestResults[1];
 
-    // // should we calculate minAmountOut? should slippage be a parameter on trade config?
-    // const minAmountOut = amountOut.sub(amountOut.mul(3).div(100));
 
-    // console.log('[Dexes] Calculated min amount', utils.formatUnits(amountOut, outDecimals), outSymbol);
-    // const executeTx = await tradeFactory.populateTransaction['execute((address,address,address,uint256,uint256),address,bytes)'](
-    //   {
-    //     _strategy: strategy,
-    //     _tokenIn: tokenInAddress,
-    //     _tokenOut: tokenOutAddress,
-    //     _amount: amount,
-    //     _minAmountOut: minAmountOut,
-    //   },
-    //   swapperAddress,
-    //   swapperData
-    // );
+    const amountOut = lastSwapResponse.amountOut;
 
-    // if (amountOut.eq(0)) throw new Error(`No ${outSymbol} tokens were received`);
+    const minAmountOut = amountOut.sub(amountOut.mul(3).div(100));
 
-    // return executeTx;
+    const data = mergeTransactions(dexesBestResults.map((result => result.unsignedSwapTx)));
+
+    console.log('[Dexes] Calculated min amount', utils.formatUnits(amountOut, outDecimals), outSymbol);
+
+    // we need to use the helper function used on CrvYfiEth to compare tx and use execute(detail) or execute(detail[])
+    const executeTx = await tradeFactory.populateTransaction['execute((address,address,address,uint256,uint256),address,bytes)'](
+      {
+        _strategy: strategy,
+        _tokenIn: tokenInAddress,
+        _tokenOut: tokenOutAddress,
+        _amount: amount,
+        _minAmountOut: minAmountOut,
+      },
+      multicallSwapperAddress,
+      data
+    );
+
+    if (amountOut.eq(0)) throw new Error(`No ${outSymbol} tokens were received`);
+
+    return executeTx;
+
   }
 }
