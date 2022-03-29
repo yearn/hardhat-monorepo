e55aff6c3a4ad3390aab56140dc345a17fb1248f
b0dhidharma <b0dhidharma@protonmail.com>
2022-03-29 15:41:30 -0300

    refactor: migrate yswaps and contract utils to external repositories

--- packages/contract-utils/.mocharc.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
-module.exports = {
-  require: ['hardhat/register'],
-  extension: ['.ts'],
-  ignore: ['./test/utils/**'],
-  recursive: true,
-};
--- packages/contract-utils/contracts/interfaces/utils/IMigratable.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.4 <0.9.0;
-
-interface IMigratable {
-  event Migrated(address _to);
-
-  function migratedTo() external view returns (address _to);
-}
